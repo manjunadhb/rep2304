f7aa8805d12c340cadde69f621198b6784c7c5f4
Sushil <pratapsushil@gmail.com>
2023-07-27 23:15:30 +1000

    second commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <h1>This line is added by Manjunadh from his local system</h1>
       <h1>This line is added by any of other team members</h1>
       <h1>Hello Everyone this line is updated by Sushil 23.10</h1>
+      <h1>adding one more time by Sushil</h1>
     </div>
   );
 }
