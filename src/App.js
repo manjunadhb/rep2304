9c52777a29af97043571fcefef77a4dc51c17540
Sushil <pratapsushil@gmail.com>
2023-07-27 23:10:27 +1000

    sushil line added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <h1>This line is added by Manjunadh from his local system</h1>
-    <h1>This line is added by any of other team members</h1>
+      <h1>This line is added by any of other team members</h1>
+      <h1>Hello Everyone this line is updated by Sushil 23.10</h1>
     </div>
   );
 }
